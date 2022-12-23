b49606815dcc80319936947ddbb14d88109e7141
hoanprd <99530243+hoanprd@users.noreply.github.com>
2022-12-23 16:38:24 +0700

    CF 23.12.2022 P1

---
+++ Mana of Zexel - The rise of the Mana/Assets/Script/Intro/SplashController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    bool ta = true;
+    float ct;
+    public float sm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ct = sm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ta == true)
+        {
+            ct -= Time.deltaTime;
+        }
+        if (ct < 0)
+        {
+            SceneManager.LoadScene("Intro");
+        }
+    }
+}
