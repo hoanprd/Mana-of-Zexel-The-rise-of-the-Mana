702bf661c30b6083bd8c22fb0cd26b1caa8e521e
hoanprd <99530243+hoanprd@users.noreply.github.com>
2023-02-05 12:36:43 +0700

    CF 05.02.2023 P1

--- Mana of Zexel - The rise of the Mana/Assets/Script/Battle/BattleSystem/PlayerBattle.cs
+++ Mana of Zexel - The rise of the Mana/Assets/Script/Battle/BattleSystem/PlayerBattle.cs
@@ -15,6 +15,10 @@ public class PlayerBattle : MonoBehaviour
     public int yes4=0;
     public int yes5=0;
     public int yes6=0;
+
+    public int yes7 = 0, yes8 = 0;
+    public int yes9 = 0, yes10 = 0;
+    public int yes11 = 0, yes12 = 0;
     public int dead1=0;
     public int dead2=0;
     public int dead3=0;
@@ -40,20 +44,49 @@ public class PlayerBattle : MonoBehaviour
             Invoke("delayP1", 1.2f);
             yes2 = 0;
         }
-        if(yes3 == 1)
+        if (yes7 == 1)
+        {
+            ani.SetTrigger("p1skill2");
+            yes7 = 0;
+        }
+        if (yes8 == 1)
+        {
+            P1.position = new Vector2(P1.position.x + 5f, P1.position.y);
+            ani.SetTrigger("p1skill3");
+            Invoke("delayP1", 1.2f);
+            yes8 = 0;
+        }
+
+        if (yes3 == 1)
         {
             P2.position = new Vector2(P2.position.x + 8f, P2.position.y - 1.5f);
             anip2.SetTrigger("p2attack");
-            Invoke("delayP2", 1.2f);
+            Invoke("delayP2", 2f);
             yes3 = 0;
         }
         if(yes4 == 1)
         {
             P2.position = new Vector2(P2.position.x + 8f, P2.position.y - 1.5f);
             anip2.SetTrigger("p2skill");
-            Invoke("delayP2", 1.2f);
+            Invoke("delayP2", 2f);
             yes4 = 0;
         }
+        if (yes9 == 1)
+        {
+            P2.position = new Vector2(P2.position.x + 8f, P2.position.y - 1.5f);
+            anip2.SetTrigger("p2skill2");
+            Invoke("delayP2", 2f);
+            yes9 = 0;
+        }
+        if (yes10 == 1)
+        {
+            P2.position = new Vector2(P2.position.x + 8f, P2.position.y - 1.5f);
+            anip2.SetTrigger("p2skill3");
+            Invoke("delayP2", 2.2f);
+            yes10 = 0;
+        }
+
+
         if (yes5 == 1)
         {
             anip3.SetTrigger("p3attack");
@@ -64,6 +97,18 @@ public class PlayerBattle : MonoBehaviour
             anip3.SetTrigger("p3skill");
             yes6 = 0;
         }
+        if (yes11 == 1)
+        {
+            anip3.SetTrigger("p3skill2");
+            yes11 = 0;
+        }
+        if (yes12 == 1)
+        {
+            anip3.SetTrigger("p3skill3");
+            yes12 = 0;
+        }
+
+
         if (dead1 == 1)
         {
             ani.SetBool("p1dead", true);
