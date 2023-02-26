fca5eaec4b0765f696fdf91e693b2f0ed4b2682d
hoanprd <99530243+hoanprd@users.noreply.github.com>
2023-02-26 22:43:37 +0700

    CF 26.02.2023 P1

--- Mana of Zexel - The rise of the Mana/Assets/Script/Menu/MenuController.cs
+++ Mana of Zexel - The rise of the Mana/Assets/Script/Menu/MenuController.cs
@@ -395,7 +395,7 @@ public class MenuController : MonoBehaviour
         SynthesizeSystem.ManaIceSynthesize = 100;
 
         SceneManager.LoadScene("Cutscenes");
-        //SceneManager.LoadScene("BSSicxalon 1");
+        //SceneManager.LoadScene("BSBlueManaSlime 1");
     }
 
     public void PressLoadGame()
